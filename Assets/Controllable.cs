2e00157268e958ff619d008bd978e1d77893be5c
wuw94 <weslew1@uci.edu>
2016-06-11 14:37:38 -0700

    testing network

---
+++ Assets/Controllable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controllable : MonoBehaviour {
+    private float moveSpeed = 2.0f;
+	void Start()
+    {
+	
+	}
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = transform.position.z;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        }
+    }
+}
